97db7dcf8427457ed59becc41417cc234c8d0fa2
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2023-11-28 23:52:38 -0500

    Fixing Crud for cloud

--- src/routes/rimmind.ts
+++ src/routes/rimmind.ts
@@ -1,168 +1,193 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import pool from '../postdb'
-import { CheckID, validate } from '../Misc/CommonFunctions';
-import { log } from 'console';
-import { CreateUserInCloud, EditRecordInCloud, cloudWrite, writeDataToCloud } from '../Misc/Children/CloudWrite';
-var no_of_writes = 0
+import { Router, Request, Response, NextFunction } from "express";
+import pool from "../postdb";
+import { CheckID, validate } from "../Misc/CommonFunctions";
+import { log } from "console";
+import {
+  CreateUserInCloud,
+  EditRecordInCloud,
+  cloudWrite,
+  deleteinCloud,
+  writeDataToCloud,
+} from "../Misc/Children/CloudWrite";
+var no_of_writes = 0;
 const router = Router();
 // get All records
-router.get('/', async (req: Request, res: Response) => {
-    const email = req.query.email as string;
+router.get("/", async (req: Request, res: Response) => {
+  const email = req.query.email as string;
 
-    if (!email) {
-        return res.status(400).json({ error: 'Email parameter is missing' });
-    }
-    try {
-        const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
-        console.log(existingUser.rows[0].id);
-        console.log("fethicng this uers data");
+  if (!email) {
+    return res.status(400).json({ error: "Email parameter is missing" });
+  }
+  try {
+    const existingUser = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+    console.log(existingUser.rows[0].id);
+    console.log("fethicng this uers data");
 
-        const userRecords = await pool.query('SELECT * FROM userrecords WHERE user_email_id = $1', [existingUser.rows[0].id]);
-        console.log(userRecords.rows);
+    const userRecords = await pool.query(
+      "SELECT * FROM userrecords WHERE user_email_id = $1",
+      [existingUser.rows[0].id]
+    );
+    console.log(userRecords.rows);
 
-        return res.status(200).send(userRecords.rows);
-    } catch (error) {
-
-    }
-    res.json({ message: `Received email: ${email}` });
+    return res.status(200).send(userRecords.rows);
+  } catch (error) {}
+  res.json({ message: `Received email: ${email}` });
 });
-// create user  
-router.post('/', async (req: Request, res: Response) => {
-    console.log("came to login");
-    
-    try {
-        const { email } = req.body;
-        console.log(`email recevied from app is ${email}`);
+// create user
+router.post("/", async (req: Request, res: Response) => {
+  console.log("came to login");
 
-        const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+  try {
+    const { email } = req.body;
+    console.log(`email recevied from app is ${email}`);
 
-        if (existingUser.rows.length > 0) {
-            return res.status(200).send('exists');
-        }
+    const existingUser = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
 
-        const result = await pool.query('INSERT INTO users (email) VALUES ($1)', [email]);
-
-        res.status(200).send('new');
-      
-        CreateUserInCloud(email)
-        return null
-    } catch (error) {
-        console.error('Error inserting data:', error);
-        res.status(500).send('Internal Server Error');
+    if (existingUser.rows.length > 0) {
+      return res.status(200).send("exists");
     }
+
+    const result = await pool.query("INSERT INTO users (email) VALUES ($1)", [
+      email,
+    ]);
+
+    res.status(200).send("new");
+
+    CreateUserInCloud(email);
+    return null;
+  } catch (error) {
+    console.error("Error inserting data:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 // Upload
-router.post('/add', async (req: Request, res: Response) => {
-    const id = req.query.id as string;
+router.post("/add", async (req: Request, res: Response) => {
+  const id = req.query.id as string;
 
-    console.log(`come here to delete the record from userrecord with id - ${id}`);
+  console.log(`come here to delete the record from userrecord with id - ${id}`);
 
-    const { title, user, desp, TagArray } = req.body;
-    try {
-        await pool.query('BEGIN');
-        // Assuming "User" table has 'id' as primary key
-        const userQueryResult = await pool.query('SELECT id FROM "users" WHERE email = $1', [user]);
+  const { title, user, desp, TagArray } = req.body;
+  try {
+    await pool.query("BEGIN");
+    // Assuming "User" table has 'id' as primary key
+    const userQueryResult = await pool.query(
+      'SELECT id FROM "users" WHERE email = $1',
+      [user]
+    );
 
-        if (userQueryResult.rows.length === 0) {
-            // User not found, you may want to handle this case
-            return res.status(404).json({ message: 'User not found' });
-        }
+    if (userQueryResult.rows.length === 0) {
+      // User not found, you may want to handle this case
+      return res.status(404).json({ message: "User not found" });
+    }
 
-        const userId = userQueryResult.rows[0].id;
+    const userId = userQueryResult.rows[0].id;
 
-        // Inserting data into "UserRecords" table
-        const insertQuery = `
+    // Inserting data into "UserRecords" table
+    const insertQuery = `
           INSERT INTO "userrecords" (user_email_id, title, description, tags, media)
           VALUES ($1, $2, $3, $4, $5)
         `;
 
-        const insertValues = [userId, title, desp, TagArray, null]; // Assuming media is not included in this example
+    const insertValues = [userId, title, desp, TagArray, null]; // Assuming media is not included in this example
 
-        await pool.query(insertQuery, insertValues);
+    await pool.query(insertQuery, insertValues);
 
-        await pool.query('COMMIT');
-        console.log("inserted into local");
-        no_of_writes++
-       
-        console.log(`current no of record inserted ${no_of_writes}`);
-        res.json({ message: 'Data added successfully' });
-        const data:cloudWrite = {
-            id:userId,
-            title:title, user:user, desp:desp, TagArray:TagArray
-        }
-            // time to backup data!
-            writeDataToCloud(data);
+    await pool.query("COMMIT");
+    console.log("inserted into local");
+    no_of_writes++;
 
-        return null
-    } catch (error) {
-        await pool.query('ROLLBACK');
-        throw error;
-    }
+    console.log(`current no of record inserted ${no_of_writes}`);
+    res.json({ message: "Data added successfully" });
+    const data: cloudWrite = {
+      id: userId,
+      title: title,
+      user: user,
+      desp: desp,
+      TagArray: TagArray,
+    };
+    // time to backup data!
+    writeDataToCloud(data);
 
+    return null;
+  } catch (error) {
+    await pool.query("ROLLBACK");
+    throw error;
+  }
 });
 // Delete Record
-router.delete('/', async (req: Request, res: Response) => {
-    const id = req.query.id as string;
-    console.log(`Came to delete the record with id - ${id}`);
-    const { title, user, desp, TagArray } = req.body;
-    try {
-        // Delete the row from the 'userrecords' table based on the provided 'id'
-        const deleteResult = await pool.query('DELETE FROM userrecords WHERE id = $1', [id]);
+router.delete("/", async (req: Request, res: Response) => {
+  const id = req.query.id as string;
+  console.log(`Came to delete the record with id - ${id}`);
+  const { title, user, desp, TagArray } = req.body;
+  try {
+    // Delete the row from the 'userrecords' table based on the provided 'id'
+    const deleteResult = await pool.query(
+      "DELETE FROM userrecords WHERE id = $1",
+      [id]
+    );
 
-        // Check if any row was deleted
-        if (deleteResult.rowCount && deleteResult.rowCount > 0) {
-            res.status(200).json({ message: 'Record deleted successfully' });
-            const data = {
-                id:id,
-                title:title,  desp:desp, TagArray:TagArray,
-                user:""
-            }
-            EditRecordInCloud(data)
-            return null
-        } else {
-            return res.status(404).json({ message: 'Record not found' });
-        }
-    } catch (error) {
-        console.error('Error deleting record:', error);
-        return res.status(500).json({ message: 'Internal Server Error' });
+    // Check if any row was deleted
+    if (deleteResult.rowCount && deleteResult.rowCount > 0) {
+      res.status(200).json({ message: "Record deleted successfully" });
+      const data = {
+        id: id,
+        title: title,
+        desp: desp,
+        TagArray: TagArray,
+        user: "",
+      };
+      deleteinCloud(data);
+      return null;
+    } else {
+      return res.status(404).json({ message: "Record not found" });
     }
-
+  } catch (error) {
+    console.error("Error deleting record:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 // edit record
 
-router.put('/', async (req: Request, res: Response) => {
-    console.log("came to update record");
-    const id = req.query.id as string;
-    try {
-        const userId = req.params.id;
-        const { title, desp, TagArray } = req.body;
-        const result = await pool.query(
-            'UPDATE userrecords SET title = $1, description = $2, tags = $3 WHERE id = $4',
-            [title, desp, TagArray, id]
-        );
+router.put("/", async (req: Request, res: Response) => {
+  console.log("came to update record");
+  const id = req.query.id as string;
+  try {
+    const userId = req.params.id;
+    const { title, desp, TagArray } = req.body;
+    const result = await pool.query(
+      "UPDATE userrecords SET title = $1, description = $2, tags = $3 WHERE id = $4",
+      [title, desp, TagArray, id]
+    );
 
-        if (result.rowCount && result.rowCount > 0) {
-            // If rowCount is greater than 0, the update was successful
+    if (result.rowCount && result.rowCount > 0) {
+      // If rowCount is greater than 0, the update was successful
 
-            res.status(200).send('updated');
-           
-            const data = {
-                id:id,
-                title:title,  desp:desp, TagArray:TagArray,
-                user:""
-            }
-            EditRecordInCloud(data)
-        } else {
-            // If rowCount is 0, no record was updated (user with the given ID not found)
-            return res.status(404).send('User not found');
-        }
-    } catch (error) {
+      res.status(200).send("updated");
 
-        console.error('Error updating user record:', error);
-        res.status(500).send('Internal Server Error');
+      const data = {
+        id: id,
+        title: title,
+        desp: desp,
+        TagArray: TagArray,
+        user: "",
+      };
+      EditRecordInCloud(data);
+    } else {
+      // If rowCount is 0, no record was updated (user with the given ID not found)
+      return res.status(404).send("User not found");
     }
+  } catch (error) {
+    console.error("Error updating user record:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 router.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(404).send('Wrong URL');
+  res.status(404).send("Wrong URL");
 });
 export default router;
