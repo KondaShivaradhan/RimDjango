6b3e1b713d12f88953a6f2779ca577e71c38114d
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2024-03-03 17:44:46 -0500

    trying for excalicloud

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import rimRouter from "./routes/rimmind";
 import excalRouter from "./routes/excali";
 import cors from "cors";
 import { rateLimit } from "express-rate-limit";
+import { log } from "console";
 
 const app = express();
 // Specify the allowed origins
@@ -19,29 +20,44 @@ const limiter = rateLimit({
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   // store: ... , // Use an external store for consistency across multiple server instances.
 });
+const myMiddleware = (req: Request, res: Response, next: Function) => {
+  // Perform some logic or actions here
 
-// Apply the rate limiting middleware to all requests.
+  next();
+};
 app.use(limiter);
-// Enable CORS with specific origins
+app.use(express.json());
+
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (
+        !origin ||
+        allowedOrigins.includes(origin) ||
+        origin.startsWith("chrome-extension")
+      ) {
+        console.log("Valid cors");
+
         callback(null, true);
       } else {
+        console.log(origin);
         callback(new Error("Not allowed by CORS"));
+        app.use((req: Request, res: Response, next: NextFunction) => {
+          res.status(404).send("Wrong URL");
+        });
+
+        app.use(
+          (err: Error, req: Request, res: Response, next: NextFunction) => {
+            console.error(err.stack);
+            res.status(500).json({ error: "Something went wrong!" });
+          }
+        );
       }
     },
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
 );
-const myMiddleware = (req: Request, res: Response, next: Function) => {
-  // Perform some logic or actions here
-
-  next();
-};
-app.use(express.json());
 
 app.get("/", myMiddleware, (req: Request, res: Response) => {
   res.send("html");
--- src/routes/excali.ts
+++ src/routes/excali.ts
@@ -1,11 +1,93 @@
 import { Router, Request, Response, NextFunction } from "express";
-
+import { UserFB, db } from "../utils/firebase";
+const crypto = require("crypto");
 const router = Router();
 
-router.get("/", async (req: Request, res: Response) => {
-  console.log("====================================");
-  console.log("Came here");
-  console.log("====================================");
+// Function to hash a password with salt
+const hashPassword = (password: string, salt: string): string => {
+  const hashedPassword = crypto
+    .pbkdf2Sync(password, salt, 1000, 64, `sha512`)
+    .toString(`hex`);
+  return hashedPassword;
+};
+
+// Function to compare hashed passwords
+const comparePasswords = (
+  password: string,
+  hashedPassword: string,
+  salt: string
+): boolean => {
+  const newlyHashedPassword = hashPassword(password, salt);
+  return hashedPassword === newlyHashedPassword;
+};
+router.post("/signup", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  console.log("came to signup with " + email, password);
+  try {
+    await db
+      .collection("User")
+      .doc(email)
+      .get()
+      .then(async (querySnapshot: any) => {
+        if (querySnapshot.empty) {
+          console.log("No User found Creating");
+          try {
+            // Assuming `email` and `pass` are provided as variables
+            const hashedPassword = hashPassword(password, email);
+            const newUser: UserFB = {
+              data: [],
+              email: email,
+              ispaid: false,
+              pass: hashedPassword,
+            };
+            // Save the user record in Firebase
+            await db.collection("User").doc(email).set(newUser);
+            res.send({
+              status: "success",
+              response: "User signed up successfully",
+            });
+          } catch (error) {
+            console.log(error);
+            res.status(501).send("Something went wrong");
+          }
+        } else {
+          res.send({
+            status: "failed",
+            response: "User already exists",
+          });
+        }
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(501).send("Something wrong");
+  }
+});
+router.post("/login", async (req: Request, res: Response) => {
+  const { email, pass } = req.body;
+  console.log("Ex Came to login with user" + email + " and pass " + pass);
+  try {
+    await db
+      .collection("User")
+      .doc(email)
+      .get()
+      .then((querySnapshot: any) => {
+        if (querySnapshot.empty) {
+          console.log("No matching document found");
+          res.send({ status: "No user" });
+        } else {
+          const data = querySnapshot.data() as UserFB;
+          if (data.pass === hashPassword(pass, email)) {
+            res.send({ status: "Auth Passed", data });
+          } else {
+            res.send({ status: "Auth Failed" });
+          }
+          console.log(querySnapshot.id, " => ", data);
+        }
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(501).send("Something wrong");
+  }
 });
 router.use((req: Request, res: Response, next: NextFunction) => {
   res.status(404).send("Wrong URL");
